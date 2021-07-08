b789f544cd265ebeafc76b76bc195f5206a2ca6a
yashss1 <yash493031@gmail.com>
2021-07-08 13:07:06 +0530

    Navigation Drawer (App Crash)

---
+++ app/src/main/java/com/example/android/navigationdrawer1/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.android.navigationdrawer1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                Fragment fragment = null;
+                switch (id){
+                    case R.id.search:
+                        fragment = new SearchFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.basket:
+                        fragment = new BasketFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.favorite:
+                        fragment = new FavFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.orders:
+                        fragment = new OrderFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.settings:
+                        fragment = new SettingsFragment();
+                        loadFragment(fragment);
+                        break;
+                    case R.id.support:
+                        fragment = new SupportFragment();
+                        loadFragment(fragment);
+                        break;
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction  = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment).commit();
+        drawerLayout.closeDrawer(GravityCompat.START);
+        fragmentTransaction.addToBackStack(null);
+    }
+}
